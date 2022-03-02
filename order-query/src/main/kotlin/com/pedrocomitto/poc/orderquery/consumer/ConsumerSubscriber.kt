3529d6fe466af1139d577e150abd32a80ea5cc22
Pedro  Comitto <pedrocomitto@gmail.com>
2022-03-02 14:50:21 -0300

    working

--- order-query/src/main/kotlin/com/pedrocomitto/poc/orderquery/consumer/ConsumerSubscriber.kt
+++
@@ -1,8 +0,0 @@
-package com.pedrocomitto.poc.orderquery.consumer
-
-import org.apache.kafka.clients.consumer.ConsumerRecord
-
-interface ConsumerSubscriber<T> {
-
-    fun process(consumerRecord: ConsumerRecord<String, T>)
-}
